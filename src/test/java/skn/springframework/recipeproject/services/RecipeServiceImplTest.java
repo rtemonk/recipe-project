5b337921e895875d3bf5b102ce2d76904ad764d0
nightsaken <antuan555@yandex.ru>
2024-11-05 10:18:32 +0300

    adding basic junit test w/ mockito

---
+++ src/test/java/skn/springframework/recipeproject/services/RecipeServiceImplTest.java
@@ -0,0 +1,42 @@
+package skn.springframework.recipeproject.services;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import skn.springframework.recipeproject.models.Recipe;
+import skn.springframework.recipeproject.repositories.RecipeRepository;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+
+class RecipeServiceImplTest {
+
+    RecipeServiceImpl recipeService;
+
+    @Mock
+    RecipeRepository recipeRepository;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+        recipeService = new RecipeServiceImpl(recipeRepository);
+    }
+
+    @Test
+    void getRecipes() throws Exception {
+        Recipe recipe = new Recipe();
+        HashSet recipesData = new HashSet();
+        recipesData.add(recipe);
+
+        when(recipeRepository.findAll()).thenReturn(recipesData);
+
+        Set<Recipe> recipes = recipeService.getRecipes();
+        assertEquals(recipes.size(), 1);
+        verify(recipeRepository, times(1)).findAll();
+    }
+}
